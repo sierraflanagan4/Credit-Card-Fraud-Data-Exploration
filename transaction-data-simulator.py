d1e8143cfed5eb88e6e32e276b890de589b8d12b
Sierra Flanagan <91637973+sierraflanagan4@users.noreply.github.com>
2021-10-27 13:11:34 +0100

    Create transaction-data-simulator.py

---
+++ transaction-data-simulator.py
@@ -0,0 +1,5 @@
+# Code Modified from Machine Learning For Credit Card Fraud Detection Practical Handbook
+# https://fraud-detection-handbook.github.io/fraud-detection-handbook/Chapter_3_GettingStarted/SimulatedDataset.html
+
+#Creating Customer Profiles
+
